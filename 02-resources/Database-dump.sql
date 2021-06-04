70d08154555b4c99c77243121fb192629f2ea2c5
BeBoX <bbojanabebox@gmail.com>
2021-06-04 12:51:36 +0200

    Database dump

--- 02-resources/Database-dump.sql
+++ 02-resources/Database-dump.sql
@@ -19,10 +19,14 @@ CREATE TABLE IF NOT EXISTS `administrator` (
   `password_hash` varchar(128) NOT NULL DEFAULT '0',
   PRIMARY KEY (`administrator_id`),
   UNIQUE KEY `uq_administrator_username` (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb3;
 
--- Dumping data for table aplikacija.administrator: ~0 rows (approximately)
+-- Dumping data for table aplikacija.administrator: ~3 rows (approximately)
 /*!40000 ALTER TABLE `administrator` DISABLE KEYS */;
+INSERT INTO `administrator` (`administrator_id`, `username`, `password_hash`) VALUES
+	(1, 'BeBox', '$2b$11$McIQqKKSFvGamtneQK86oO8C1kIxILsbT6MRdTMr6UNXuc1cc5RPu'),
+	(3, 'BeBox1', '$2b$11$HIRvEgj9INbAsddKzwq5IuVnYIiKhIhjyaUJ3AnQNvoQAybJEVndO'),
+	(5, 'Bojana', '$2b$11$hFN1klnLuW.fJ1m5/UPRBujI3t.rLXBN2hOaXUVyh.pSa.XhBLHyS');
 /*!40000 ALTER TABLE `administrator` ENABLE KEYS */;
 
 -- Dumping structure for table aplikacija.article
@@ -38,10 +42,13 @@ CREATE TABLE IF NOT EXISTS `article` (
   PRIMARY KEY (`article_id`),
   KEY `fk_article_category_id` (`category_id`),
   CONSTRAINT `fk_article_category_id` FOREIGN KEY (`category_id`) REFERENCES `category` (`category_id`) ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb3;
 
--- Dumping data for table aplikacija.article: ~0 rows (approximately)
+-- Dumping data for table aplikacija.article: ~2 rows (approximately)
 /*!40000 ALTER TABLE `article` DISABLE KEYS */;
+INSERT INTO `article` (`article_id`, `name`, `category_id`, `excerpt`, `description`, `status`, `is_promoted`, `created_at`) VALUES
+	(1, 'samo ti', 1, 'neki opis', 'opis za prikazivanje', 'available', 1, '2021-05-30 15:53:18'),
+	(4, 'samo ti', 1, 'neki opis', 'opis za prikazivanje', 'available', 1, '2021-05-30 21:21:06');
 /*!40000 ALTER TABLE `article` ENABLE KEYS */;
 
 -- Dumping structure for table aplikacija.article_feature
@@ -51,14 +58,17 @@ CREATE TABLE IF NOT EXISTS `article_feature` (
   `feature_id` int(10) unsigned NOT NULL DEFAULT 0,
   `value` varchar(255) NOT NULL DEFAULT '0',
   PRIMARY KEY (`article_feature_id`),
-  UNIQUE KEY `uq_article_feature_article_id_feature_id` (`article_id`,`feature_id`),
-  KEY `fk_article_feature_feature_id` (`feature_id`),
+  UNIQUE KEY `uq_article_feature_article_id_feature_id` (`article_id`,`feature_id`) USING BTREE,
+  KEY `fk_article_feature_feature_id` (`feature_id`) USING BTREE,
   CONSTRAINT `fk_article_feature_article_id` FOREIGN KEY (`article_id`) REFERENCES `article` (`article_id`) ON UPDATE CASCADE,
   CONSTRAINT `fk_article_feature_feature_id` FOREIGN KEY (`feature_id`) REFERENCES `feature` (`feature_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb3;
 
--- Dumping data for table aplikacija.article_feature: ~0 rows (approximately)
+-- Dumping data for table aplikacija.article_feature: ~2 rows (approximately)
 /*!40000 ALTER TABLE `article_feature` DISABLE KEYS */;
+INSERT INTO `article_feature` (`article_feature_id`, `article_id`, `feature_id`, `value`) VALUES
+	(1, 1, 1, '4'),
+	(4, 4, 1, '3');
 /*!40000 ALTER TABLE `article_feature` ENABLE KEYS */;
 
 -- Dumping structure for table aplikacija.article_price
@@ -70,10 +80,13 @@ CREATE TABLE IF NOT EXISTS `article_price` (
   PRIMARY KEY (`article_price_id`),
   KEY `fk_article_price_id_article_id` (`article_id`),
   CONSTRAINT `fk_article_price_id_article_id` FOREIGN KEY (`article_id`) REFERENCES `article` (`article_id`) ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb3;
 
--- Dumping data for table aplikacija.article_price: ~0 rows (approximately)
+-- Dumping data for table aplikacija.article_price: ~2 rows (approximately)
 /*!40000 ALTER TABLE `article_price` DISABLE KEYS */;
+INSERT INTO `article_price` (`article_price_id`, `article_id`, `price`, `created_at`) VALUES
+	(1, 1, 10.00, '2021-05-30 15:53:48'),
+	(4, 4, 142.44, '2021-05-30 21:21:06');
 /*!40000 ALTER TABLE `article_price` ENABLE KEYS */;
 
 -- Dumping structure for table aplikacija.cart
@@ -84,10 +97,15 @@ CREATE TABLE IF NOT EXISTS `cart` (
   PRIMARY KEY (`cart_id`),
   KEY `fk_cart_user_id` (`user_id`),
   CONSTRAINT `fk_cart_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb3;
 
--- Dumping data for table aplikacija.cart: ~0 rows (approximately)
+-- Dumping data for table aplikacija.cart: ~4 rows (approximately)
 /*!40000 ALTER TABLE `cart` DISABLE KEYS */;
+INSERT INTO `cart` (`cart_id`, `user_id`, `created_at`) VALUES
+	(1, 1, '2021-06-01 10:30:33'),
+	(2, 5, '2021-06-01 10:31:25'),
+	(3, 5, '2021-06-01 11:30:30'),
+	(4, 6, '2021-06-04 10:53:03');
 /*!40000 ALTER TABLE `cart` ENABLE KEYS */;
 
 -- Dumping structure for table aplikacija.cart_article
@@ -101,10 +119,13 @@ CREATE TABLE IF NOT EXISTS `cart_article` (
   KEY `fk_cart_article_id_article_id` (`article_id`),
   CONSTRAINT `fk_cart_article_id_article_id` FOREIGN KEY (`article_id`) REFERENCES `article` (`article_id`) ON UPDATE CASCADE,
   CONSTRAINT `fk_cart_article_id_cart_id` FOREIGN KEY (`cart_id`) REFERENCES `cart` (`cart_id`) ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb3;
 
--- Dumping data for table aplikacija.cart_article: ~0 rows (approximately)
+-- Dumping data for table aplikacija.cart_article: ~2 rows (approximately)
 /*!40000 ALTER TABLE `cart_article` DISABLE KEYS */;
+INSERT INTO `cart_article` (`cart_article_id`, `cart_id`, `article_id`, `quantity`) VALUES
+	(1, 2, 1, 5),
+	(4, 4, 4, 1);
 /*!40000 ALTER TABLE `cart_article` ENABLE KEYS */;
 
 -- Dumping structure for table aplikacija.category
@@ -118,10 +139,15 @@ CREATE TABLE IF NOT EXISTS `category` (
   UNIQUE KEY `uq_category_image_path` (`image_path`),
   KEY `fk_category_parent_category_id` (`parent_category_id`),
   CONSTRAINT `fk_category_parent_category_id` FOREIGN KEY (`parent_category_id`) REFERENCES `category` (`category_id`) ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb3;
 
--- Dumping data for table aplikacija.category: ~0 rows (approximately)
+-- Dumping data for table aplikacija.category: ~4 rows (approximately)
 /*!40000 ALTER TABLE `category` DISABLE KEYS */;
+INSERT INTO `category` (`category_id`, `name`, `image_path`, `parent_category_id`) VALUES
+	(1, 'Prva', '/static/image/1.png', NULL),
+	(2, 'Druga(1)', '/static/image/2.png', 1),
+	(6, 'Vuneni Tepisi', 'http://slika.com/tepih.png', NULL),
+	(7, 'Deciji Tepisi', 'http://slika.com/tepihd.png', NULL);
 /*!40000 ALTER TABLE `category` ENABLE KEYS */;
 
 -- Dumping structure for table aplikacija.feature
@@ -133,10 +159,13 @@ CREATE TABLE IF NOT EXISTS `feature` (
   UNIQUE KEY `uq_feature_name_category_id` (`name`,`category_id`),
   KEY `fk_feature_category_id` (`category_id`),
   CONSTRAINT `fk_feature_category_id` FOREIGN KEY (`category_id`) REFERENCES `category` (`category_id`) ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb3;
 
--- Dumping data for table aplikacija.feature: ~0 rows (approximately)
+-- Dumping data for table aplikacija.feature: ~2 rows (approximately)
 /*!40000 ALTER TABLE `feature` DISABLE KEYS */;
+INSERT INTO `feature` (`feature_id`, `name`, `category_id`) VALUES
+	(2, 'poliester', 2),
+	(1, 'vuneni', 1);
 /*!40000 ALTER TABLE `feature` ENABLE KEYS */;
 
 -- Dumping structure for table aplikacija.order
@@ -148,10 +177,12 @@ CREATE TABLE IF NOT EXISTS `order` (
   PRIMARY KEY (`order_id`),
   UNIQUE KEY `uq_order_cart_id` (`cart_id`),
   CONSTRAINT `fk_order_cart_id` FOREIGN KEY (`cart_id`) REFERENCES `cart` (`cart_id`) ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
 
--- Dumping data for table aplikacija.order: ~0 rows (approximately)
+-- Dumping data for table aplikacija.order: ~1 rows (approximately)
 /*!40000 ALTER TABLE `order` DISABLE KEYS */;
+INSERT INTO `order` (`order_id`, `created_at`, `cart_id`, `status`) VALUES
+	(1, '2021-06-01 11:16:40', 2, 'pending');
 /*!40000 ALTER TABLE `order` ENABLE KEYS */;
 
 -- Dumping structure for table aplikacija.photo
@@ -163,10 +194,13 @@ CREATE TABLE IF NOT EXISTS `photo` (
   UNIQUE KEY `uq_photo_image_path` (`image_path`),
   KEY `fk_photo_article_id` (`article_id`),
   CONSTRAINT `fk_photo_article_id` FOREIGN KEY (`article_id`) REFERENCES `article` (`article_id`) ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb3;
 
--- Dumping data for table aplikacija.photo: ~0 rows (approximately)
+-- Dumping data for table aplikacija.photo: ~2 rows (approximately)
 /*!40000 ALTER TABLE `photo` DISABLE KEYS */;
+INSERT INTO `photo` (`photo_id`, `article_id`, `image_path`) VALUES
+	(1, 1, '/static/pic1.png'),
+	(3, 4, 'static/uploads/cb0ee761-4b33-4b92-be31-288a8b8e6769-2021/05/cb0ee761-4b33-4b92-be31-288a8b8e6769-1.png');
 /*!40000 ALTER TABLE `photo` ENABLE KEYS */;
 
 -- Dumping structure for table aplikacija.user
@@ -174,17 +208,21 @@ CREATE TABLE IF NOT EXISTS `user` (
   `user_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `email` varchar(25) NOT NULL DEFAULT '0',
   `password_hash` varchar(128) NOT NULL DEFAULT '0',
-  `forname` varchar(64) NOT NULL DEFAULT '0',
+  `forename` varchar(64) NOT NULL DEFAULT '0',
   `surname` varchar(64) NOT NULL DEFAULT '0',
   `phone_number` varchar(24) NOT NULL DEFAULT '',
   `postal_address` text NOT NULL,
   PRIMARY KEY (`user_id`),
   UNIQUE KEY `uq_user_email` (`email`),
   UNIQUE KEY `uq_user_phone_number` (`phone_number`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb3;
 
--- Dumping data for table aplikacija.user: ~0 rows (approximately)
+-- Dumping data for table aplikacija.user: ~3 rows (approximately)
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` (`user_id`, `email`, `password_hash`, `forename`, `surname`, `phone_number`, `postal_address`) VALUES
+	(1, 'botaeyeon4@gmail.com', '$2b$11$zg8gcGz/yrB30ICUjxHIROsVH5wpBbc7ARl1Ke83c6ShhmyHMeooO', 'Taeyeon', 'Bo', '062468468', 'Tokyo 94'),
+	(5, 'bowanderlust4@gmail.com', '$2b$11$xDGh3jLMCROQ8UhYYHugaOEvyK26buRC.hwD9qu18oc/Oz1o1i9wK', 'Bo', 'Bo', '0230482482', 'Seoul 18'),
+	(6, 'aya@gmail.com', '$2b$11$2mmPuFIQUqeEmmtP1g0c2OLMoeFCj9eNeDPVYu5mUt1D/HQr17u3W', 'Aya', 'Brea', '0230444444', 'I live where I want');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
